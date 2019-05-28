f424eee99fab970a38902fbfef6e7c29d5f36763
Leander Gebhardt <legebharle.west@web.de>
2019-05-28 13:37:18 +0200

    Main einmal überarbeitet

--- src/Fabrik/Main.java
+++ src/Fabrik/Main.java
@@ -2,40 +2,71 @@ package Fabrik;
 
 public class Main {
 
-
+	private static SchuhHersteller enki = new Enki();
+	private static SchuhHersteller didadas = new Didadas();
+	private static SchuhHersteller umap = new Umap();
+	
+	/*
+	 * Indicies to pass into erzeugeSchuhe
+	 * 
+	 * 1 - Sportschuhe
+	 * 2 - Stiefel
+	 * 3 - Wanderschuhe
+	 */
+	
+	public final static Schuh[] EnkiFactory() {
+		Schuh[] enkiSchuhe = new Schuh[3];
+		
+		for (int i = 0; i < enkiSchuhe.length; i++) {
+			enkiSchuhe[i] =  enki.erzeugeSchuhe(i + 1);
+		}
+		
+		return enkiSchuhe;
+	}
+	
+	public final static Schuh[] didadasFactory() {
+		Schuh[] didadasSchuhe = new Schuh[3];
+		
+		for (int i = 0; i < didadasSchuhe.length; i++) {
+			didadasSchuhe[i] =  didadas.erzeugeSchuhe(i + 1);
+		}
+		
+		return didadasSchuhe;
+	}
+	
+	public final static Schuh[] umapFactory() {
+		Schuh[] umapSchuhe = new Schuh[3];
+		
+		for (int i = 0; i < umapSchuhe.length; i++) {
+			umapSchuhe[i] =  umap.erzeugeSchuhe(i + 1);
+		}
+		
+		return umapSchuhe;
+	}
+	
 	public static void main(String[] args) {
-		
-		/*
-		 * Indicies to pass into erzeugeSchuhe
-		 * 
-		 * 1 - Sportschuhe
-		 * 2 - Stiefel
-		 * 3 - Wanderschuhe
-		 */
-		
-		SchuhHersteller hersteller = new Enki();
-		Schuh meinSchuh = hersteller.erzeugeSchuhe(1);
-
-		SchuhHersteller hersteller2 = new Didadas();
-		Schuh zweitSchuh = hersteller2.erzeugeSchuhe(2);
-		
-		SchuhHersteller hersteller3 = new Enki();
-		Schuh drittSchuh = hersteller3.erzeugeSchuhe(3);
-		
+				
 		// console output to check whats going on
-		System.out.println(printShoe(meinSchuh));
-		System.out.println(printShoe(zweitSchuh));
-		System.out.println(printShoe(drittSchuh));
+		System.out.println(printShoe(EnkiFactory()));
+		System.out.println(printShoe(didadasFactory()));
+		System.out.println(printShoe(umapFactory()));
 
 	}
 
-	private static String printShoe(Schuh schoe){
-		return  ("- Schuhgröße: " + schoe.getGroeße()
-				+ ", Preis: " + schoe.getPreis()
-				+ "€, Farbe: " + schoe.getFarbe()
-				+ ", Marke: " + schoe.getMarke()
-				+ ", ClassType: " + schoe.getClass())
-				+ "\n";
+	private static String printShoe(Schuh[] schoe){
+		
+		String result = " " + schoe[0].getMarke() + "\n";
+		
+		for (int i = 0; i < schoe.length; i++) {
+			result += "- Schuhgröße: " + schoe[i].getGroeße()
+			+ ", Preis: " + schoe[i].getPreis()
+			+ "€, Farbe: " + schoe[i].getFarbe()
+			+ ", Marke: " + schoe[i].getMarke()
+			+ ", ClassType: " + schoe[i].getClass()
+			+ "\n";
+		}
+		
+		return result;
 	}
 
 }
