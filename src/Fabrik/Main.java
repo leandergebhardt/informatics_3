b760abe087e3579114b37faf7f47bcc150ff5474
Leander Gebhardt <legebharle.west@web.de>
2019-05-28 12:22:11 +0200

    Dritten Schuh in Main

--- src/Fabrik/Main.java
+++ src/Fabrik/Main.java
@@ -19,18 +19,23 @@ public class Main {
 		SchuhHersteller hersteller2 = new Didadas();
 		Schuh zweitSchuh = hersteller2.erzeugeSchuhe(2);
 		
+		SchuhHersteller hersteller3 = new Enki();
+		Schuh drittSchuh = hersteller3.erzeugeSchuhe(3);
+		
 		// console output to check whats going on
 		System.out.println(printShoe(meinSchuh));
 		System.out.println(printShoe(zweitSchuh));
+		System.out.println(printShoe(drittSchuh));
 
 	}
 
 	private static String printShoe(Schuh schoe){
-		return  (" Schuhgröße: " + schoe.getGroeße()
+		return  ("- Schuhgröße: " + schoe.getGroeße()
 				+ ", Preis: " + schoe.getPreis()
 				+ "€, Farbe: " + schoe.getFarbe()
 				+ ", Marke: " + schoe.getMarke()
-				+ ", ClassType: " + schoe.getClass());
+				+ ", ClassType: " + schoe.getClass())
+				+ "\n";
 	}
 
 }
