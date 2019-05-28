96d8d5048c84d68363768bf3d74c8dc96ae50fbb
Leander Gebhardt <legebharle.west@web.de>
2019-05-28 12:36:22 +0200

    operation in SchuhHersteller entfernt

--- src/Fabrik/SchuhHersteller.java
+++ src/Fabrik/SchuhHersteller.java
@@ -5,10 +5,7 @@ public abstract class SchuhHersteller {
 	public SchuhHersteller() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void operation() {
-	}
-	
+		
 	public abstract Schuh erzeugeSchuhe(int Type);
 
 }
